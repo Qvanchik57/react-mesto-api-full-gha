570d9f0a4b99cbbc99efa6d309872886f9eab7c6
Sergey Роднуля <116501676+Qvanchik57@users.noreply.github.com>
2024-01-25 16:04:36 +0300

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import logo from '../images/header-logo.svg';
 import {Link} from 'react-router-dom';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
+import userEvent from '@testing-library/user-event';
 
 function Header(props) {
     const sign = `${props.headerSign}`;
-
+    const user = React.useContext(CurrentUserContext);
+    
     function handleOutClick() {
       localStorage.removeItem('jwt');
       props.handleLoginOut();
@@ -15,7 +19,7 @@ function Header(props) {
           
           <img className="header__logo" alt="Логотип мест" src={logo} />
           <div className='header__profile'>
-            {props.loggedIn && <p className='header__email'>{props.headerEmail}</p>}
+            {props.loggedIn && <p className='header__email'>{user.email}</p>}
             <Link to={sign} className='header__link' onClick={handleOutClick}>{props.headerLink}</Link>
           </div>
         </header>
